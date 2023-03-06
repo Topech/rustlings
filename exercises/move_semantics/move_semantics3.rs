467d8ba174eff6ec6997df73d4595e593266f9cf
Topech <benjaminlongbottom98@gmail.com>
2023-03-06 21:10:54 +0800

    progress

--- exercises/move_semantics/move_semantics3.rs
+++ exercises/move_semantics/move_semantics3.rs
@@ -3,21 +3,24 @@
 // (no lines with multiple semicolons necessary!)
 // Execute `rustlings hint move_semantics3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn main() {
-    let vec0 = Vec::new();
+    let mut vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(&mut vec0);
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
     vec1.push(88);
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+
+    // testing that vec0 can be used once finished with vec1.
+    // this will not work if the below line occurs before the final println! above
+    println!("{} has length {} content `{:?}`", "vec1", vec0.len(), vec0);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+fn fill_vec(vec: &mut Vec<i32>) -> &mut Vec<i32> {
     vec.push(22);
     vec.push(44);
     vec.push(66);
